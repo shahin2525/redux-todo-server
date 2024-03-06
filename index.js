c24a2b32570554f37595c3aca826947dbe5795bf
shahin2525 <shahin451999@gmail.com>
2024-03-05 22:36:17 -0800

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb://localhost:27017`;
+// const uri =
+//   "mongodb+srv://<username>:<password>@cluster0.ax6qyiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -20,8 +22,8 @@ const client = new MongoClient(uri, {
 const run = async () => {
   // await client.connect();
   try {
-    const db = client.db('todo');
-    const taskCollection = db.collection('tasks');
+    const db = client.db("todo");
+    const taskCollection = db.collection("tasks");
 
     // app.get('/tasks', async (req, res) => {
     //   const cursor = taskCollection.find({});
@@ -29,7 +31,7 @@ const run = async () => {
     //   res.send({ status: true, data: tasks });
     // });
 
-    app.get('/tasks', async (req, res) => {
+    app.get("/tasks", async (req, res) => {
       let query = {};
       if (req.query.priority) {
         query.priority = req.query.priority;
@@ -39,20 +41,20 @@ const run = async () => {
       res.send({ status: true, data: tasks });
     });
 
-    app.post('/task', async (req, res) => {
+    app.post("/task", async (req, res) => {
       const task = req.body;
       const result = await taskCollection.insertOne(task);
       res.send(result);
     });
 
-    app.get('/task/:id', async (req, res) => {
+    app.get("/task/:id", async (req, res) => {
       const id = req.params.id;
       const result = await taskCollection.findOne({ _id: ObjectId(id) });
       // console.log(result);
       res.send(result);
     });
 
-    app.delete('/task/:id', async (req, res) => {
+    app.delete("/task/:id", async (req, res) => {
       const id = req.params.id;
       const result = await taskCollection.deleteOne({ _id: ObjectId(id) });
       // console.log(result);
@@ -60,7 +62,7 @@ const run = async () => {
     });
 
     // status update
-    app.put('/task/:id', async (req, res) => {
+    app.put("/task/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const task = req.body;
@@ -83,8 +85,8 @@ const run = async () => {
 
 run().catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
